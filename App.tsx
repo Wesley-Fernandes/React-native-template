97dd0ae04fc02f5f61f035b12e21540296e94a20
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-09-12 16:26:03 -0300

    feat: tab navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Homepage from './routes/Homepage';
+import Searchpage from './routes/Searchpage';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Inicio" component={Homepage} />
+        <Tab.Screen name="Procurar" component={Searchpage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ routes/Historypage.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Historypage() {
+  return (
+    <View>
+      <Text>Historypage</Text>
+    </View>
+  )
+}
